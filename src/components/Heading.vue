159915961720ddb3278f71997974617adbd8c25e
Rich King <rking@rebgv.org>
2021-03-12 16:49:58 -0800

    Fixing line height for <Heading>

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -2,7 +2,7 @@
 
   <div class="my-8 sm:flex sm:items-center">
     <div class="bg-rebgv-yellow w-36 h-1.5 mb-8 sm:w-1.5 sm:h-16 sm:mb-0"></div>
-    <h1 class="font-serif sm:ml-5">{{ title }}</h1>
+    <h1 class="font-serif leading-tight sm:ml-5">{{ title }}</h1>
   </div>
 
 </template>
