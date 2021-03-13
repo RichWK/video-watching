89d3bc4fb76412c7b8b7f3f9609e7d5b76d790ef
Rich King <rking@rebgv.org>
2021-03-12 16:49:18 -0800

    Adding props to routes and <Video>

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,15 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Home from "../views/Home.vue";
-import Video from "../views/Video.vue";
-import VideoData from '../assets/videos.json'
-import { RoutesType } from '../types'
-import { convertToUrl, removeSpecialCharacters } from '../helpers'
+import Home from "./views/Home.vue";
+import Video from "./views/Video.vue";
+import VideoData from './assets/videos.json'
+import { RoutesType } from './types'
+import { convertToUrl, removeSpecialCharacters } from './helpers'
 
 let routes: RoutesType = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   }
 ];
 
@@ -18,10 +18,13 @@ VideoData.forEach( ( video ) =>
     { 
       path: '/' + convertToUrl( video.name ),
       name: removeSpecialCharacters( video.name ),
-      component: Video
+      component: Video,
+      props: video
     }
   )
 );
+
+console.log( routes );
 
 const router = createRouter({
   history: createWebHistory(),
--- src/views/Video.vue
+++ src/views/Video.vue
@@ -1,15 +1,33 @@
 <template>
 
-  <h1>Video</h1>
+  <Heading :title="name" />
 
 </template>
 
 <script lang="ts">
 
 import { defineComponent } from 'vue'
+import Heading from '../components/Heading.vue'
 
 export default defineComponent({
-  name: 'Video'
+  name: 'Video',
+  components: {
+    Heading
+  },
+  props: {
+    duration: {
+      type: String,
+      required: true
+    },
+    imageName: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
 })
 
 </script>
