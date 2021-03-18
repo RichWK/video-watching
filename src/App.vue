4cf3c4a612d9ccf1eb2a3643d985ccfb174ed0a6
Rich King <rking@rebgv.org>
2021-03-17 17:20:10 -0700

    Added warning regarding this being an alpha build

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
 
+  <p
+    @click=""
+    class="bg-purple-800 text-center text-white opacity-50 select-none py-1 pb-2"
+  >
+    This site is an alpha build and subject to change.
+  </p>
   <div class="mx-auto inset-x-0 max-w-screen-xxl">
     <Navigation class="sm:ml-8 lg:ml-16" />
     <div class="px-6 sm:px-8 lg:px-16 pb-16 xl:pb-24">
