7a853d47c557a730a046ad00927ddbab70c74ab5
Rich King <rking@rebgv.org>
2021-03-17 17:19:31 -0700

    Simulating existing site's navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="flex sm:inline-block">
+  <div class="flex">
     <div class="bg-rebgv-green px-10 py-4 sm:px-12 sm:py-8">
       <img
         alt="Logo for the Real Estate Board of Greater Vancouver"
@@ -11,6 +11,10 @@
     <div class="flex items-center justify-end flex-grow bg-rebgv-pale-green pr-6 sm:hidden">
       <p class="font-serif text-rebgv-gray cursor-pointer p-4">Menu</p>
     </div>
+    <ul class="hidden flex-grow sm:block text-rebgv-green text-sm font-bold text-right px-8 lg:px-16 py-3">
+      <li class="inline">Account</li>
+      <li class="inline ml-3">Contact</li>
+    </ul>
   </div>
 
 </template>
